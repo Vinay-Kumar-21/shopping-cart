5f4e92d010fb2d8dfc50d7531667d43314f2ea28
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-03-05 21:27:52 +0530

    cart-app created and routing done

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+    return (
+        <div style={{ display: "flex", alignItems: "center", justifyContent: 'space-between' }}>
+            <span className='logo'>Redux store</span>
+            <div>
+                <Link className="navLink" to="/">Home</Link>
+                <Link className='navLink' to="/cart">Cart</Link>
+                <span className='cartCount'>Cart items: 0</span>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
