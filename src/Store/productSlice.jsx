8da1b3607af687f5154dec0086b89e722a17289a
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-03-07 23:27:11 +0530

    toolkit

--- src/Store/productSlice.jsx
+++ src/Store/productSlice.jsx
@@ -8,13 +8,22 @@ const productSlice = createSlice({
         data: [],
         status: "success"
     },
-    reducers: {
-        setProducts(state, action) {
-            return { ...state, data: action.payload }
-        },
-        setStatus(state, action) {
-            return { ...state, status: action.payload }
-        }
+    // reducers: {
+    //     setProducts(state, action) {
+    //         return { ...state, data: action.payload }
+    //     },
+    //     setStatus(state, action) {
+    //         return { ...state, status: action.payload }
+    //     }
+    // }
+    extraReducers: (builder) => {
+        builder.addCase(fetchProducts.pending, (state) => {
+            return { ...state, status: "loading" }
+        }).addCase(fetchProducts.fulfilled, (state, action) => {
+            return { data: action.payload, status: "success" }
+        }).addCase(fetchProducts.rejected, (state) => {
+            return { ...state, status: "error" }
+        })
     }
 
 })
@@ -25,16 +34,23 @@ export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
 //thunk
-export function fetchProducts() {
-    return async function (dispatch) {
-        dispatch(setStatus("loading"))
-        try {
-            let res = await axios.get('https://fakestoreapi.com/products');
-            dispatch(setProducts(res.data));
-            dispatch(setStatus("success"));
-        } catch (error) {
-            dispatch(setStatus("error"));
-        }
+// export function fetchProducts() {
+//     return async function (dispatch) {
+//         dispatch(setStatus("loading"))
+//         try {
+//             let res = await axios.get('https://fakestoreapi.com/products');
+//             dispatch(setProducts(res.data));
+//             dispatch(setStatus("success"));
+//         } catch (error) {
+//             dispatch(setStatus("error"));
+//         }
 
-    }
-}
+//     }
+// }
+
+
+//create thunk with toolkit
+export const fetchProducts = createAsyncThunk('product', async () => {
+    let res = await axios.get('https://fakestoreapi.com/products');
+    return res.data;
+})
